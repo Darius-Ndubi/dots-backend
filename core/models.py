d0e15368bb644ec81a92fdfd76d042afd3775ea1
Nasir Hussain <nasirhjafri@gmail.com>
2020-04-20 19:05:44 +0500

    #4 Added workspace model

--- core/models.py
+++ core/models.py
@@ -5,3 +5,10 @@ from django.utils.translation import gettext_lazy as _
 
 class User(AbstractUser):
     title = models.CharField(_('title'), max_length=200, blank=True)
+
+
+class Workspace(models.Model):
+    name = models.CharField(_('name'), max_length=200)
+    description = models.TextField(blank=True)
+    url = models.URLField(max_length=500)
+    workspace_url = models.SlugField(unique=True)
