b225ad06e02400c8860eb91542162012ef671ab6
Nasir Hussain <nasirhjafri@gmail.com>
2020-05-02 18:13:16 +0500

    Added api url

--- dots/urls.py
+++ dots/urls.py
@@ -18,16 +18,18 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from dots import settings
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title='Dots Core API',
-      default_version='v1',
-      description="Dots Core API",
-      terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
-      license=openapi.License(name="BSD License"),
-   )
+    openapi.Info(
+        title='Dots Core API',
+        default_version='v1',
+        description="Dots Core API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License")
+    ),
+    url=settings.SWAGGER_SETTINGS.get('DEFAULT_API_URL')
 )
 
 urlpatterns = [
