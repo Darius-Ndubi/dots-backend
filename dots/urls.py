69883b6063f0f1f18017b1ea9d6b0ee4939c3963
Nasir Hussain <nasirhjafri@gmail.com>
2020-05-02 17:58:53 +0500

    Updated swagger description

--- dots/urls.py
+++ dots/urls.py
@@ -23,7 +23,7 @@ schema_view = get_schema_view(
    openapi.Info(
       title='Dots Core API',
       default_version='v1',
-      description="Test description",
+      description="Dots Core API",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
