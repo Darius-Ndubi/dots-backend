a026a66971e090b48021ba5ed6a10d6c4b15a399
Nasir Hussain <nasirhjafri@gmail.com>
2020-05-15 16:28:59 +0500

    #38 Seed staff users

--- .docker/start.sh
+++ .docker/start.sh
@@ -14,6 +14,10 @@ echo "+++++++++++++++++ Make migrations ++++++++++++++++++++++++"
 python3 manage.py migrate
 echo "------------------- Make migrations Done !!! ------------------------"
 
+echo "+++++++++++++++++ Make staff users ++++++++++++++++++++++++"
+python3 manage.py make_staff_users
+echo "------------------- Make staff users Done !!! ------------------------"
+
 echo "+++++++++++++++++ Start up the server ++++++++++++++++++++++++"
 gunicorn --workers 2  dots.wsgi -b 0.0.0.0:8000 --access-logfile '-'
 echo "------------------- Development server up and running ------------------------"
